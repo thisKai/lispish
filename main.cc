01e5ac2e1e72286565128a4e5592441be2cbb44a
Kai Patrick <thiskaiguy@gmail.com>
2017-07-16 16:31:28 +0100

    Create a REPL that does nothing

--- main.cc
+++ main.cc
@@ -1,4 +1,11 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+
 int main(int argc, char* argv[])
 {
-
+    char* line;
+    while(line != "exit"){
+        line = readline("repl> ");
+        add_history(line);
+    }
 }
