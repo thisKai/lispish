9f347a9266911873a2f0451e44bc4bb7ed7ad33e
Kai Patrick <thiskaiguy@gmail.com>
2017-07-16 17:59:13 +0100

    Use std::string in the repl

--- main.cc
+++ main.cc
@@ -1,11 +1,12 @@
+#include <string>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 int main(int argc, char* argv[])
 {
-    char* line;
+    std::string line;
     while(line != "exit"){
         line = readline("repl> ");
-        add_history(line);
+        add_history(line.c_str());
     }
 }
